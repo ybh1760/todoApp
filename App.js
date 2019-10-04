7d57037c41df18be91e1fc917aaeacd21fc9792a
Yang <ybh1760@gmail.com>
2019-10-04 17:37:56 +0900

    기본 디자인만들기-title,card 부분

--- App.js
+++ App.js
@@ -1,19 +1,51 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  TextInput,
+  Dimensions
+} from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+const { width, height } = Dimensions.get("window");
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <Text style={styles.title}>ToDo List</Text>
+        <View style={styles.card}>
+          <TextInput style={styles.input} placeholder="input todos" />
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "red",
-    alignItems: "center",
-    justifyContent: "center"
+    backgroundColor: "#F23657",
+    alignItems: "center"
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "600",
+    color: "white",
+    marginTop: 90,
+    marginBottom: 60
+  },
+  card: {
+    width: width - 40,
+    height,
+    backgroundColor: "white",
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    elevation: 5
+  },
+  input: {
+    width
   }
 });
